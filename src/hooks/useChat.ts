154482b4845f26a6417166d2c2fc648b36335224
nadavlie <117371222+nadavlie@users.noreply.github.com>
2022-12-29 15:14:08 +0200

    starting the project

--- src/hooks/useChat.ts
+++ src/hooks/useChat.ts
@@ -1,7 +1,13 @@
 import { useEffect, useState } from "react";
-import { Message } from '../types/message';
-import { User } from '../types/user';
-import { addNewMessage, changeMessageLikes, getMessages, getUserDetails, getUsers } from './server-requests';
+import { Message } from "../types/message";
+import { User } from "../types/user";
+import {
+  addNewMessage,
+  changeMessageLikes,
+  getMessages,
+  getUserDetails,
+  getUsers,
+} from "./server-requests";
 
 export function useChat() {
   const [users, setUsers] = useState<User[]>([]);
@@ -29,7 +35,7 @@ export function useChat() {
   };
 
   const addMessage = async (event: any) => {
-    if (event.key === 'Enter' && event.target.value) {
+    if (event.key === "Enter" && event.target.value) {
       const newMessage = {
         id: messages.length + 1, // in reality, id should be added by the server
         timestamp: new Date(),
@@ -39,17 +45,15 @@ export function useChat() {
         // todo: authorName should be added by the server
       };
 
-
       setMessages([
         ...messages,
         {
           ...newMessage,
           likes: [],
           authorName: currentUser!.name,
-          status: 'pending'
-        }
+          status: "pending",
+        },
       ]);
-
 
       // todo - bonus: handle changing the message status from 'pending' to 'ok'
       //  when a success response is returned from the server
@@ -58,12 +62,13 @@ export function useChat() {
       // todo - remove these lines - mocking changing the message status
       setTimeout(() => {
         setMessages([
-          ...messages, {
+          ...messages,
+          {
             ...newMessage,
             likes: [],
             authorName: currentUser!.name,
-            status: 'ok'
-          }
+            status: "ok",
+          },
         ]);
       }, 1000);
     }
@@ -71,7 +76,9 @@ export function useChat() {
 
   const toggleLike = async (message: Message) => {
     const userLiked = message.likes!.indexOf(currentUser!.id);
-    userLiked === -1 ? message.likes!.push(currentUser!.id) : message.likes!.splice(userLiked, 1);
+    userLiked === -1
+      ? message.likes!.push(currentUser!.id)
+      : message.likes!.splice(userLiked, 1);
     setSelectedMessage({ ...message });
 
     // todo: change the likes in the server
@@ -80,7 +87,7 @@ export function useChat() {
 
   const openAuthorDetails = async (author: User) => {
     setSelectedAuthor(author); // name and id only
-    setSelectedAuthor(await getUserDetails(author.id) || null);
+    setSelectedAuthor((await getUserDetails(author.id)) || null);
     // todo: get user details from the server
   };
 
@@ -98,5 +105,5 @@ export function useChat() {
     selectUser,
     addMessage,
     toggleLike,
-  }
+  };
 }
